d6611a8c1ead2a8467dce46d43e6cac75a3911a6
Gleb Grudiev <grudiev.g@gmail.com>
2026-03-23 22:18:27 +0200

    Fix linter warnings

--- src/common/utils/typeorm-logger.ts
+++ src/common/utils/typeorm-logger.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Logger, QueryRunner } from 'typeorm';
 import { requestContext } from '../middleware/request-context.middleware';
 
