b6570ab85e67919794bd1073147acde85b8d36ad
Gleb Grudiev <grudiev.g@gmail.com>
2026-04-30 00:20:19 +0300

    Fix broken unit tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuditService } from '../common/audit/audit.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -8,7 +9,10 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        { provide: UsersService, useValue: {} },
+        { provide: AuditService, useValue: {} },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
